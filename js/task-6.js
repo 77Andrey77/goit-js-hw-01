9dc92bcf3f6430f1192fab6760816e412d21064d
Andrii Pysmennyi <andrey.ad511989@gmail.com>
2022-07-08 09:21:32 +0300

    homework1-task6

--- js/task-6.js
+++ js/task-6.js
@@ -1,4 +1,12 @@
-let input = prompt('Введите произвольное число');
+let input;
 let total = 0;
 
 
+do {
+  input = prompt('Введите произвольное число');
+  total += Number(input);
+  console.log(total);
+} while (input !== null);
+
+
+alert(`Общая сумма чисел равна ${total}`)
